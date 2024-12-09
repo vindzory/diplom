242c592b0d70b2e574b054b92511f7be9122dff5
vindzory <amster2199@gmail.com>
2024-12-09 13:46:58 +0200

    start commit

---
+++ server.py
@@ -0,0 +1,14 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == "/":
+            self.send_response(200)
+            self.end_headers()
+            self.wfile.write(b"Hello, world!")
+
+if __name__ == "__main__":
+    server_address = ("", 8080)  # Слушаем порт 8080
+    httpd = HTTPServer(server_address, SimpleHTTPRequestHandler)
+    print("Server running on port 8080...")
+    httpd.serve_forever()
