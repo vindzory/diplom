2c7bd9113d43f522496e17b48835e494ba49b043
vindzory <amster2199@gmail.com>
2024-12-11 23:31:03 +0200

    Project setup: backend, Dockerfile, CI/CD, Terraform, and Kubernetes deployment

---
+++ eks-setup/main.tf
@@ -0,0 +1,38 @@
+provider "aws" {
+  region = "us-east-1" 
+}
+
+module "eks" {
+  source          = "terraform-aws-modules/eks/aws"
+  cluster_name    = "msych-devops3-cluster" 
+  cluster_version = "1.26"
+  subnets         = ["<10.10.10.0/24>"]
+
+  node_groups = {
+    example = {
+      desired_capacity = 1
+      max_capacity     = 1
+      min_capacity     = 1
+
+      instance_type = "t3.medium"
+    }
+  }
+}
+
+module "argocd" {
+  source  = "terraform-helm-release"
+  name    = "argocd"
+  chart   = "argo-cd"
+  version = "5.1.1"
+  namespace = "argocd"
+
+  set {
+    name  = "server.ingress.enabled"
+    value = "true"
+  }
+
+  set {
+    name  = "server.ingress.hosts[0]"
+    value = "argocd.msych.devops3.test-danit.com"
+  }
+}
--- server.py
+++ server.py
@@ -8,7 +8,7 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
             self.wfile.write(b"Hello, world!")
 
 if __name__ == "__main__":
-    server_address = ("", 8080)  # Слушаем порт 8080
+    server_address = ("", 8080) 
     httpd = HTTPServer(server_address, SimpleHTTPRequestHandler)
     print("Server running on port 8080...")
     httpd.serve_forever()
